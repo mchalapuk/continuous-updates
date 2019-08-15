93c281d7311f7e7dc1ac54070a43bbbb0e5f82a3
Maciej Chałapuk <maciej@chalapuk.pl>
2019-08-16 01:56:17 +0200

    + cloning repos

--- run.js
+++ run.js
@@ -1,5 +1,8 @@
 
 const yaml = require('js-yaml');
+const git = require('simple-git');
+const mkdir = require('mkdirp');
+const rimraf = require('rimraf');
 const fs = require('fs');
 
 const check = require('offensive').default;
@@ -8,24 +11,71 @@ require('offensive/assertions/fieldThat/register');
 require('offensive/assertions/aString/register');
 require('offensive/assertions/allElementsThat/register');
 
-cwd(__dirname);
-const config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf-8'));
+const WORKSPACE = `${__dirname}/workspace/`;
 
-console.info('Validating config.yml');
-check(config, 'config')
-  .has.fieldThat('pkgs', field => field
-    .contains.allElementsThat(elem => elem
-      .has.fieldThat('name', name => name.is.aString)
-      .and.fieldThat('repoUrl', repoUrl => repoUrl.is.aString)
-      .and.fieldThat('pwdVar', pwdVar => pwdVar.is.aString)
-      .and.fieldThat('testTask', testTask => testTask.is.aString)
-    )
-  )
-;
-console.info('[success]');
+cwd(__dirname);
+const config = readConfigFile();
+
+runAll(config.pkgs, 0);
+
+function log(msg) {
+  process.stdout.write(msg);
+}
 
 function cwd(dir) {
-  console.info(`cwd: ${__dirname}`);
+  console.info(`cwd: ${dir}`);
   process.cwd(dir);
+}
+
+function readConfigFile() {
+  const config = yaml.safeLoad(fs.readFileSync('./config.yml', 'utf-8'));
+
+  log('Validating config.yml');
+  check(config, 'config')
+    .has.fieldThat('pkgs', field => field
+      .contains.allElementsThat(elem => elem
+        .has.fieldThat('name', name => name.is.aString)
+        .and.fieldThat('repoUrl', repoUrl => repoUrl.is.aString)
+        .and.fieldThat('pwdVar', pwdVar => pwdVar.is.aString)
+        .and.fieldThat('testTask', testTask => testTask.is.aString)
+      )
+    )
+    ()
+  ;
+  log(' [success]\n');
+  return config;
+}
+
+function runAll(pkgs, index) {
+  if (index === pkgs.length) {
+    return;
+  }
+  run(pkgs[index])
+    .then(
+      () => runAll(pkgs, index + 1),
+      err => {
+        console.error(err);
+        system.exit(1);
+      },
+    )
+  ;
+}
+
+async function run(pkg) {
+  log(`--- ${pkg.name} ---\n`);
+  cwd(WORKSPACE);
+
+  const folder = `${WORKSPACE}${pkg.name}`;
+  log(`Removing folder: ${folder}`);
+  rimraf.sync(folder);
+  log(' [success]\n');
+
+  const repo = git().silent(true);
+
+  log(`Cloning ${pkg.repoUrl}`);
+  await repo.clone(pkg.repoUrl, folder);
+  log(' [success]\n');
+
+  cwd(folder);
 }
 
