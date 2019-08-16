4303489e7d161f5cc3ac5425e7970eaed7e1ebb7
Maciej Chałapuk <m.chalapuk@quedex.net>
2019-08-16 14:51:30 +0200

    + fixed loggins in run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ PWD_VAR=$3
 TEST_TASK=$4
 DEPLOY_TASK=$5
 
-echo "\n--- ${PKG_NAME} ---\n"
+echo ""
+echo "--- ${PKG_NAME} ---"
+echo ""
 
 FOLDER="./${PKG_NAME}"
 echo -n "Removing folder: ${FOLDER}"
@@ -18,15 +20,17 @@ echo " [success]"
 echo -n "Adding ssh keys"
 KEY_FILE="../keys/${PKG_NAME}"
 eval "KEY_PASS=\$${PWD_VAR}"
-eval $(ssh-agent)
-ssh-add -D
-openssl rsa -in ${KEY_FILE} -passin pass:${KEY_PASS} -out ./key
+eval $(ssh-agent) > /dev/null
+ssh-add -D > /dev/null
+openssl rsa -in ${KEY_FILE} -passin pass:${KEY_PASS} -out ./key > /dev/null
 chmod 0600 ./key
-ssh-add ./key
+ssh-add ./key > /dev/null
 echo " [success]"
 
-git clone $REPO_URL
+echo -n "Cloning $REPO_URL"
+git clone $REPO_URL > /dev/null
 cd $FOLDER
+echo " [success]"
 
 npm install
 OUTDATED=`npm outdated`
@@ -46,7 +50,7 @@ echo " [success]"
 
 echo -n "Checking in dependencies to git..."
 git add .
-git commit -m "~ updated depdendencies"
+git commit -m "~ updated depdendencies" >> /dev/null
 echo " [success]"
 
 echo -n "Applying new version... "
@@ -57,6 +61,6 @@ npm run $DEPLOY_TASK >/dev/null
 echo " [success]"
 
 echo -n "Pushing changes to git..."
-git push
+git push >> /dev/null
 echo " [success]"
 
